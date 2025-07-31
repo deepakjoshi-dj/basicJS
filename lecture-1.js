fc263c06dc4e82d09e7d185f468f9ce94fbabb1a
deepakjoshi-dj <dpkjoshi656.dj@gmail.com>
2025-07-31 22:40:15 +0530

    first lecture

--- lecture-1.js
+++ lecture-1.js
@@ -1,1 +1,67 @@
 console.log("hello world");
+// variables
+var a = 10;
+a = 20;
+// console.log(a);
+
+let b = 100;
+b = 200;
+// console.log(b)
+
+const x = 10;
+// console.log(x);
+
+// Operators ---------
+// Arithmetic Operators +, -, *, /, %, **, ++, --
+
+// console.log("addition : ", a + b);
+// console.log("subtraction : ", a - b);
+// console.log("multiplication : ", a * b);
+// console.log("Division : ", a / b);
+// console.log("modulus (reminder) : ", a % b);
+// console.log("Exponent : ", a ** b);
+// console.log("Increment : ", a++);
+// console.log("Decrement : ", a--);
+
+// Assignment Operators =, +=, -=, *=, /=
+
+// Comparison Operators <, >, <=, >=, ==, !=, ===, !==
+var num1 = 10;
+var num2 = 20;
+var num3 = 10;
+var str1 = "10";
+var str2 = "true";
+var bool1 = true;
+
+// console.log(num1 < num2);
+// console.log(num1 > num2);
+// console.log(num1 <= num2);
+// console.log(num1 <= num3);
+// console.log(num1 == str1);
+// console.log(num1 === str1);
+
+// Logical Operators &&, ||, !
+
+// string operator (+) concatenate.
+console.log("Hello" + " " + "World.");
+
+// Type operator typeof.
+console.log(typeof 10);
+
+// Ternary operator.
+// syntax => condition ? true : false;
+num = 10;
+const isEven = num % 2 === 0 ? "Even" : "Odd";
+
+// Conditional statement if, if -> else, if -> else if -> else, switch
+
+// Loops for, while, do->while, forEach, for..of
+
+// For loop
+// for(initialization;condition;increment){
+//       code .....
+// }
+
+for (let i = 0; i <= 5; i++) {
+  console.log(`i value is : ${i}`);
+}
