7d689a9ef77757bcc72ebfc071d5a0ad41ee75dd
deepakjoshi-dj <dpkjoshi656.dj@gmail.com>
2025-08-07 22:25:16 +0530

    dom access element and nullish coalescing

--- importantConcepts.js
+++ importantConcepts.js
@@ -20,4 +20,36 @@ greet("Alice");
 // Optional Chaining (?.) (ES11)
 const obj = { userName: "Alice", age: 34 };
 console.log(obj.userName);
+console.log(obj?.address?.city);
+console.log(obj?.contact?.phone);
 console.log(obj?.address?.city?.splice?.());
+
+const obj1 = { greet: () => "Hello" };
+console.log(obj1.greet());
+console.log(obj1?.sayHi?.());
+
+// Nullish Coalescing Operator (??)
+// Provides a default value only if the left-hand side is null or undefined.
+// Does not target for false, 0, "", or NaN
+
+let employeeName = null;
+let displayValue = employeeName ?? "Guest";
+console.log(displayValue);
+
+// ex OR(||) vs ??
+
+const count = 0;
+console.log(count ?? 10);
+console.log(count || 10);
+
+// with optional chaining;
+const student = {
+  profile: { name: "Bob" },
+};
+
+console.log(student?.profile?.age ?? 24);
+
+// chaining with multiple defaults
+
+const result = null ?? undefined ?? "Default";
+console.log(result);
